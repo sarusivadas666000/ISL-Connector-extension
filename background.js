c8ab0b3f077e70c97d2816a3e32abf3e35e6bf62
elsastiny <117434618+elsastiny@users.noreply.github.com>
2026-06-12 10:55:24 +0530

    Delete background.js

--- background.js
+++
@@ -1,9 +0,0 @@
-// background.js
-chrome.action.onClicked.addListener((tab) => {
-  // Launches the panel window on the right side of the active browser tab
-  chrome.sidePanel.setOptions({
-    tabId: tab.id,
-    path: 'panel.html',
-    enabled: true
-  });
-});
